12b77c118188cc746d2c4ef66e689222610a3c1b
Mateus Felipe <brasil.mateus748@gmail.com>
2025-05-04 19:54:31 -0300

    Initial commit

---
+++ src/main/java/com/app/email/EmailWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWebAppApplication.class, args);
+	}
+
+}
