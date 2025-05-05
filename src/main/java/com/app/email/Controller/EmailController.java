74075f686c2be40aa94e8dfa818f95863c4f2199
Mateus Felipe <brasil.mateus748@gmail.com>
2025-05-05 00:04:48 -0300

    Create email controller and DTO files

---
+++ src/main/java/com/app/email/Controller/EmailController.java
@@ -0,0 +1,11 @@
+package com.app.email.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @PostMapping
+
+}
---
+++ src/main/java/com/app/email/DTO/EmailDTO.java
@@ -0,0 +1,5 @@
+package com.app.email.DTO;
+
+public class EmailDTO {
+
+}
